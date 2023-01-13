0e97def56fb5e2bf0d852ce1047e55983e0fea1e
corazon4815 <mango4815@gmail.com>
2023-01-13 18:32:26 +0900

    feat : ProviderUtil 파일 추가

---
+++ oembed/src/main/java/com/web/oembed/provider/ProviderUtil.java
@@ -0,0 +1,60 @@
+package com.web.oembed.provider;
+
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONException;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class ProviderUtil {
+
+    public String getYoutubeOembedUrl(String url) throws IOException, JSONException {
+        //log.info("OembedServiceimpl.youtubeHandler");
+        String top = "https://www.youtube.com/oembed?url=https%3A//youtube.com/watch%3Fv%3D";
+        String mid = url.split("watch\\?v=")[1];  //watch?v=
+        String botm = "&format=json";
+        String resultUrl = top + mid + botm;
+        System.out.println(resultUrl);
+        //String jsonData = getJsonObject(result);
+        return resultUrl;
+//    return  jsonhandler.youtubeJson(jsonData);
+    }
+
+    public String getInstagramOembedUrl(String url) throws IOException, JSONException {
+        //log.info("OembedServiceimpl.youtubeHandler");
+        String top = "https://www.youtube.com/oembed?url=https%3A//youtube.com/watch%3Fv%3D";
+        String mid = url.split("watch\\?v=")[1];  //watch?v=
+        String botm = "&format=json";
+        String resultUrl = top + mid + botm;
+        System.out.println(resultUrl);
+        //String jsonData = getJsonObject(result);
+        return resultUrl;
+//    return  jsonhandler.youtubeJson(jsonData);
+    }
+
+    public String getTwitterOembedUrl(String url) throws IOException, JSONException {
+        //log.info("OembedServiceimpl.youtubeHandler");
+        String top = "https://www.youtube.com/oembed?url=https%3A//youtube.com/watch%3Fv%3D";
+        String mid = url.split("watch\\?v=")[1];  //watch?v=
+        String botm = "&format=json";
+        String resultUrl = top + mid + botm;
+        System.out.println(resultUrl);
+        //String jsonData = getJsonObject(result);
+        return resultUrl;
+//    return  jsonhandler.youtubeJson(jsonData);
+    }
+
+    public String getVimeoOembedUrl(String url) throws IOException, JSONException {
+        //log.info("OembedServiceimpl.youtubeHandler");
+        String top = "https://www.youtube.com/oembed?url=https%3A//youtube.com/watch%3Fv%3D";
+        String mid = url.split("watch\\?v=")[1];  //watch?v=
+        String botm = "&format=json";
+        String resultUrl = top + mid + botm;
+        System.out.println(resultUrl);
+        //String jsonData = getJsonObject(result);
+        return resultUrl;
+//    return  jsonhandler.youtubeJson(jsonData);
+    }
+
+}
